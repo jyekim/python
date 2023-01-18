01eac631adc79812040911948b40cc4f2f857acb
jyekim <121778189+jyekim@users.noreply.github.com>
2023-01-18 23:58:26 +0900

    Add files via upload

--- import numpy.py
+++ import numpy.py
@@ -9,3 +9,11 @@ from scipy import stats
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 x = stats.mode(speed)
 print(x)
+
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.var(speed)
+
+print(x)
